101a88acfd25743a6410b5e007559ffcda31a85e
cyseria <xcyseria@gmail.com>
2019-04-11 11:43:48 +0800

    chore(simple): 更改 dll 配置路径

--- webpack4-mobx-simple-page/build/webpack.dll.config.js
+++ webpack4-mobx-simple-page/build/webpack.dll.config.js
@@ -5,15 +5,16 @@ module.exports = {
         vendor: ['react', 'react-dom', 'react-router-dom', 'mobx', 'mobx-react']
     },
     output: {
-        path: path.resolve(__dirname, 'dll'),
+        path: path.resolve(__dirname, '../dll'),
         filename: '[name].dll.[hash:5].js',
         library: '[name]_library'
     },
     plugins: [
         new webpack.DllPlugin({
             name: '[name]_library',
-            path: path.resolve(__dirname, 'dll', 'manifest.json'),
-            context: __dirname
+            path: path.resolve(__dirname, '../dll', 'manifest.json'),
+             // manifest缓存文件的请求上下文（默认为webpack执行环境上下文）
+            context: process.cwd()
         })
     ]
 };
